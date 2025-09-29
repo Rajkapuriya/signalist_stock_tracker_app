9c647c68253b95abc2e43a00e8a66c4954638892
Raj Kapuriya <rajkapuriya03@gmail.com>
2025-09-29 14:28:29 +0530

    feat/implement auth logic

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,15 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import NavItems from '@/components/NavItems';
-import UserDropDown from './UserDropDown';
+import UserDropdown from '@/components/UserDropdown';
 
-const Header = () => {
+const Header = ({ user }: { user: User }) => {
   return (
     <header className="sticky top-0 header">
       <div className="container header-wrapper">
         <Link href="/">
           <Image
-            src="assets/icons/logo.svg"
+            src="/assets/icons/logo.svg"
             alt="Signalist logo"
             width={140}
             height={32}
@@ -19,7 +19,8 @@ const Header = () => {
         <nav className="hidden sm:block">
           <NavItems />
         </nav>
-        <UserDropDown />
+
+        <UserDropdown user={user} />
       </div>
     </header>
   );
